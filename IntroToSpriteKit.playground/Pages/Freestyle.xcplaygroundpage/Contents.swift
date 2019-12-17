2bae5a3ec585be6e02bc44dba5fff57d09ebd524
WilliamChan <wchan@lcs.on.ca>
2019-12-17 15:28:52 -0500

    started page 7

--- IntroToSpriteKit.playground/Pages/Freestyle.xcplaygroundpage/Contents.swift
+++ IntroToSpriteKit.playground/Pages/Freestyle.xcplaygroundpage/Contents.swift
@@ -12,6 +12,7 @@ import SpriteKit            // Provides 2D graphics classes
 // Define the animation size, scene, and scene color
 let frame = CGRect(x: 0, y: 0, width: 400, height: 300)
 var scene = SKScene(size: frame.size)
+scene.backgroundColor = .white
 
 // Define, configure, and present the scene
 let view = SKView(frame: frame)
@@ -31,6 +32,18 @@ PlaygroundSupport.PlaygroundPage.current.liveView = view
  [Previous: Adding Nodes Repeatedly](@previous) | Page 7
  
  */
+// add tennis ball node
 let tennisBall = SKSpriteNode(imageNamed: "tennis-ball")
+tennisBall.position = CGPoint(x:scene.size.width/2, y:scene.size.width/2)
+scene.addChild(tennisBall)
+
+// add 
 let tennisRacket = SKSpriteNode(imageNamed: "tennis-racket")
-tennisBall.position = GCPoint(x:scene.size.width/2, y:scene)
+tennisRacket.position = CGPoint(x:scene.size.width/2, y:scene.size.width/2)
+scene.addChild(tennisRacket)
+
+
+
+scene.physicsBody = SKPhysicsBody(edgeLoopFrom: scene.frame)
+
+
